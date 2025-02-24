7d2b68d09889bbdf0bf2acc7dcb3270b5360fc76
Rama El <ramaelmaulanaagung@gmail.com>
2025-02-24 14:47:27 +0700

    Init flask

---
+++ flask/app.py
@@ -0,0 +1,73 @@
+from flask import Flask, request
+from dotenv import load_dotenv
+import pymongo
+import os
+from utils import get_time_now, send_ubidots
+import requests as req
+
+load_dotenv()
+
+app = Flask(__name__)
+client = pymongo.MongoClient(os.getenv("MONGO_URL"))
+db = client["db"]
+
+
+@app.route("/")
+def index():
+    print(db)
+    return "Index Page"
+
+
+@app.route("/api/temp", methods=["GET", "POST"])
+def temp_api():
+    temp_collections = db["temperature"]
+    if request.method == "POST":
+        temp = request.json["temp"]
+        print(temp)
+        temp_collections.insert_one({"temp": temp, "time": get_time_now()})
+        send_ubidots({"temp": temp})
+        return f"Data sent: {temp}"
+    if request.method == "GET":
+        print(temp_collections.find_one())
+        return str(temp_collections.find())
+
+
+@app.route("/api/distance", methods=["GET", "POST"])
+def distance_api():
+    collections = db["distance"]
+    if request.method == "POST":
+        distance = request.json["distance"]
+        collections.insert_one({"distance": distance, "time": get_time_now()})
+        send_ubidots({"distance": distance})
+        return f"Data sent: {distance}"
+    if request.method == "GET":
+        return "Distance: "
+
+
+@app.route("/api/humidity", methods=["GET", "POST"])
+def humidity_api():
+    collections = db["humidity"]
+    if request.method == "POST":
+        humidity = request.json["humidity"]
+        collections.insert_one({"humidity": humidity, "time": get_time_now()})
+        send_ubidots({"humidity": humidity})
+        return f"Data sent: {humidity}"
+    if request.method == "GET":
+        return "Distance: "
+
+
+@app.route("/api/iot", methods=["POST"])
+def iot_api():
+    humidity_collections = db["humidity"]
+    temp_collections = db["temperature"]
+    distance_collections = db["distance"]
+    if request.method == "POST":
+        humidity = request.json["humidity"]
+        distance = request.json["distance"]
+        temp = request.json["temp"]
+        humidity_collections.insert_one({"humidity": humidity, "time": get_time_now()})
+        distance_collections.insert_one({"distance": distance, "time": get_time_now()})
+        temp_collections.insert_one({"temp": temp, "time": get_time_now()})
+        data = {"humidity": humidity, "distance": distance, "temp": temp}
+        send_ubidots(data)
+        return data
---
+++ flask/utils.py
@@ -0,0 +1,24 @@
+import datetime
+from dotenv import load_dotenv
+import os
+import json
+import requests as req
+
+load_dotenv()
+
+
+def get_time_now():
+    return datetime.datetime.now(tz=datetime.timezone.utc)
+
+
+def send_ubidots(data):
+    UBIDOTS_DEVICE_ID = os.getenv("UBIDOTS_DEVICE_ID")
+    UBIDOTS_TOKEN = os.getenv("UBIDOTS_TOKEN")
+    url = "http://industrial.api.ubidots.com/api/v1.6/devices/" + UBIDOTS_DEVICE_ID
+    headers = {"Content-Type": "application/json", "X-Auth-Token": UBIDOTS_TOKEN}
+    try:
+        response = req.post(url=url, data=json.dumps(data), headers=headers)
+        print("Ubidots Response:", response.text)
+        del response  # Hapus response untuk menghemat memori
+    except Exception as e:
+        print("Gagal mengirim data:", e)
